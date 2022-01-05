482084d37519575670642019585d931203ae88e2
Arief romadhon <ariefromadhon26@gmail.com>
2022-01-05 10:21:54 +0700

    add step item component

---
+++ storegg/components/molecules/StepItem/index.tsx
@@ -0,0 +1,29 @@
+export interface StepItemProps {
+  icon: 'step 1' | 'step 2' | 'step 3';
+  title: string;
+  desc1: string;
+  desc2: string;
+}
+
+export default function stepItem(props: StepItemProps) {
+  const { icon, title, desc1, desc2 } = props;
+  return (
+    <div>
+      <div>
+        <img
+          src={`/icon/${icon}.svg`}
+          alt="icon step"
+          className="mb-30"
+          width={80}
+          height={80}
+        />
+        <p className="fw-semibold text-2xl mb-2 color-palette-1">{title}</p>
+        <p className="text-lg color-palette-1 mb-0">
+          {desc1}
+          <br />
+          {desc2}
+        </p>
+      </div>
+    </div>
+  );
+}
